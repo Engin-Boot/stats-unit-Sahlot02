da1dc0bc48d07358fc1278fc977340509757c23c
Sahlot02 <40887789+Sahlot02@users.noreply.github.com>
2021-07-22 13:43:00 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Statistics
 {
-    public class StatsComputer
+    public static class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public static computedStats CalculateStatistics(List<float> numbers)
+        {
+            computedStats computedStatsValue = new computedStats();
+            if (numbers.Capacity == 0) return computedStatsValue;
+            float average = 0;
+            float min = float.MaxValue;
+            float max = float.MinValue;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+                average += numbers[i];
+
+            }
+            average = average / numbers.Count;
+            computedStatsValue.average = average;
+            computedStatsValue.max = max;
+            computedStatsValue.min = min;
+            return computedStatsValue;
         }
     }
 }
