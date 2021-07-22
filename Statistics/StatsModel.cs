9974616d6ff1951db1ccc3820be23f3fadea2a62
Sahlot02 <40887789+Sahlot02@users.noreply.github.com>
2021-07-22 13:44:40 +0530

    Add files via upload

---
+++ Statistics/StatsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Statistics
+{
+    public class computedStats
+    {
+        public float average { get; set; }
+        public float min { get; set; }
+        public float max { get; set; }
+
+        public computedStats()
+        {
+            this.average = float.NaN;
+            this.min = float.NaN;
+            this.max = float.NaN;
+        }
+    }
+
+}
